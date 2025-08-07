246508b8f3207b0affb7ccade37503e8f88cb37c
Diogo <diogo.marcondes2004@gmail.com>
2025-08-06 22:49:02 -0300

    InitialCreate

---
+++ AshAndEspresso/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AshAndEspresso.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public string? ImageUrl { get; set; }
+}
